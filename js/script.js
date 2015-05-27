b5759fbae93317e6cf3a0cac10c3c40988b2840f
Alvaro Moo <darkwin3.05@gmail.com>
2015-05-27 22:22:03 +0200

    Cambios en estructura y estilos

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	var str=location.href.toLowerCase();
+	$("nav a").each(function() {
+		if (str.indexOf(this.href.toLowerCase()) > -1) {
+			$("nav a").removeClass("activa");
+			$(this).addClass("activa");
+		}
+	});
+})
