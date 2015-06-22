b424c02a8c6681075941ccaf6683efddf38d3acc
Alvaro Moo <darkwin3.05@gmail.com>
2015-06-22 19:39:10 +0200

    Cambios generales. Nuevo directorio para librerías JS externas. Cambios en JS. Cambios en JADE. cambios en SASS.

--- js/iframe.js
+++ js/iframe.js
@@ -1,19 +1,5 @@
-// // Iframe
-// $(document).ready(function(){
-// 	var iframeOculto = '.iframe-1 iframe';
-
-// 	var iframeVisible = '.iframe-2 iframe';
-
-// 	$(iframeOculto).load(function(){
-// 		var alturaIFrame = $(this).outerHeight() + 'px';
-
-// 		$(iframeVisible).height(alturaIFrame);
-		
-// 		console.log(alturaIFrame);
-// 	});
-// });
-
-$('iframe').load(function() {
-    this.style.height =
-    this.contentWindow.document.body.offsetHeight + 'px';
+// iFrame
+$(document).ready(function(){
+    $('#iframe-1 iframe').iframeHeight();
+    // $('#iframe-2 iframe').iframeHeight();
 });
--- js/script.js
+++ js/script.js
@@ -10,57 +10,86 @@ $(document).ready(function(){
 });
 
 // Menú | Tab
+// $(document).ready(function(){
+// 	var modulo = '.menu-tab';
+
+// 	$(modulo).each(function(){
+// 		var tabs = $(this).find('.menu-tab-options');
+// 		var tab = $(tabs).find('li');
+// 		var tabLink = $(tab).find('a');
+// 		var content = $(modulo).find('.menu-tab-content > *')
+
+// 		function prevenirEnlace() {
+// 			$(tabLink).click(function (e){
+// 				e.preventDefault();
+// 			})
+// 		}
+
+// 		function definirAnchoTab() {
+// 			var numeroTabs = $(tab).length;
+// 			var anchoTab = 100 / numeroTabs + '%';
+
+// 			$(tab).width(anchoTab);
+// 		}
+
+// 		function activarPrimerTab(){
+// 			var primerTab = $(tab).first();
+
+// 			$(primerTab).addClass('activa');
+// 		}
+
+// 		function cambiarTabActivo(){
+// 			$(tab).click(function(){
+// 				$(tab).removeClass('activa');
+// 				$(this).addClass('activa')
+// 			});
+// 		}
+
+// 		function mostrarPrimerContenido() {
+// 			var firstContent = $(content).eq(0);
+			
+// 			$(content).hide();
+// 			$(firstContent).show();
+// 		}
+
+// 		// function filtrarContenido() {
+// 		// 	var primerContent = $(content).first();
+
+// 		// 	$(content).hide();
+// 		// 	$(primerContent).show();
+
+// 		// 	$(tab).click(function(){
+// 		// 		indexActual = $(this).index();
+// 		// 		contentActual = $(content).eq(indexActual);
+// 		// 		if ($(contentActual)){
+// 		// 			$(content).hide();
+// 		// 			$(contentActual).show();
+// 		// 		}
+// 		// 	})
+// 		// }
+
+// 		prevenirEnlace();
+// 		definirAnchoTab();
+// 		activarPrimerTab();
+// 		cambiarTabActivo();
+// 		mostrarPrimerContenido();
+// 		// filtrarContenido();
+// 	});
+// });
+
 $(document).ready(function(){
-	var modulo = '.menu-tab';
-	var tab = modulo + ' .menu-tab-options > li';
-	var tabLink = modulo + ' .menu-tab-options > li';
-	var content = modulo + ' .menu-tab-content > *';
+	$('[data-tab]').on('click', function (e) {
+		$(this)
+			.addClass('active')
+			.siblings('[data-tab]')
+			.removeClass('active')
+			.siblings('[data-content=' + $(this).data('tab') + ']')
+			.addClass('active')
+			.siblings('[data-content]')
+			.removeClass('active')
+			e.preventDefault()      
+    });
 
-	function prevenirEnlace() {
-		$(tabLink).click(function (e){
-			e.preventDefault();
-		})
-	}
-
-	function definirAnchoTab() {
-		var numeroTabs = $(tab).length;
-		var anchoTab = 100 / numeroTabs + '%';
-
-		$(tab).width(anchoTab);
-	}
-
-	function activarPrimerTab(){
-		var primerTab = tab + ':first-of-type';
-
-		$(primerTab).addClass('activa');
-	}
-
-	function cambiarTabActivo(){
-		$(tab).click(function(){
-			$(tab).removeClass('activa');
-			$(this).addClass('activa')
-		});
-	}
-
-	function filtrarContenido() {
-		var primerContent = content + ':first-of-type';
-
-		$(content).hide();
-		$(primerContent).show();
-
-		$(tab).click(function(){
-			indexActual = $(this).index();
-			contentActual = content + ':nth(' + indexActual + ')'
-			if ($(contentActual)){
-				$(content).hide();
-				$(contentActual).show();
-			}
-		})
-	}
-
-	prevenirEnlace();
-	definirAnchoTab();
-	activarPrimerTab();
-	cambiarTabActivo();
-	filtrarContenido();
 });
+
+
